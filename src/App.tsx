a182c2dca8f78a8a93834aa4b7fc9e78430e2921
Karera <karera@s2tmk.local>
2024-10-27 00:20:24 +0900

    feat: LIFFを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import liff from "@line/liff";
 import axios from "axios";
 import "./App.css";
 import { useQuery } from "@tanstack/react-query";
@@ -7,12 +9,39 @@ const axiosInstance = axios.create({
 });
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    liff
+      .init({
+        liffId: import.meta.env.VITE_APP_LIFF_ID,
+      })
+      .then(() => {
+        setMessage("LIFF init succeeded.");
+      })
+      .catch((e: Error) => {
+        setMessage("LIFF init failed.");
+        setError(`${e}`);
+      });
+  });
+
   const { data, isLoading } = useQuery({
     queryKey: ["message"],
     queryFn: () => axiosInstance.get("/api/hello").then((res) => res.data),
   });
 
-  return <>{isLoading ? "Loading..." : data}</>;
+  return (
+    <>
+      {isLoading ? "Loading..." : data}
+      {message && <p>{message}</p>}
+      {error && (
+        <p>
+          <code>{error}</code>
+        </p>
+      )}
+    </>
+  );
 }
 
 export default App;
